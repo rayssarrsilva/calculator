9b0fbbea4d6c2bcfb4300dfc10bd4963f6b2f39c
Rayssa Roberta <116031703+rayssarrsilva@users.noreply.github.com>
2026-05-06 12:34:37 -0300

    Call the functions in the operate function
    return a error message if the input is wrong and calculate the value as
    the operator passed expects

--- javascript.js
+++ javascript.js
@@ -15,5 +15,17 @@ const divide = function(n1, n2) {
 };
 
 const operate = function(operator, n1, n2){
-    
+    if (operator === '+'){
+        return add(n1, n1);
+    } else if (operator === '-'){
+        return subtract(n1, n2);
+    } else if (operator === '*'){
+        return multiply(n1, n2);
+    } else if (operator === '/'){
+        return divide(n1, n2);
+    } else {
+        return 'ERROR: it only accept +, -, / and * in string format plus 2 numbers';
+    }
 };
+
+console.log(operate("/", 2, 3));
