f3ef857a5514fe64f424688451f81b09afe0a499
Rayssa Roberta <116031703+rayssarrsilva@users.noreply.github.com>
2026-05-06 12:18:51 -0300

    Create the first four function expression needed
    used the function expression method to create the add, subtract,
    multiply, divide and operate function passing their parameters

---
+++ javascript.js
@@ -0,0 +1,19 @@
+const add = function(n1, n2) {
+    
+};
+
+const subtract = function(n1, n2) {
+
+};
+
+const multiply = function(n1, n2) {
+
+};
+
+const divide = function(n1, n2) {
+
+};
+
+const operate = function(operator, n1, n2){
+
+};
